648fbb2f0c731774275e7c10a03ce8ed43562f02
aa5gi <dev@boudinot.com>
2016-08-14 18:36:17 -0500

    formatting

--- first_pi_script.py
+++ first_pi_script.py
@@ -9,8 +9,10 @@ ser.flushOutput()
 
 while True:
     print ser
+
     bytesToRead = ser.inWaiting()
     readBytes   = ser.read(bytesToRead)
+    
     print "READING\n\n"
     print readBytes
     # time.sleep(0.013) # delays for 5 seconds
